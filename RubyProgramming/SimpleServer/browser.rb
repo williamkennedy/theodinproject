be6f48254b19a849cdf8127bbc003baa9841e9fe
williamkennedy <williamkennedyjnr@gmail.com>
2015-01-27 22:02:49 +0000

    Finished Web Server but must go back and revisit it

--- RubyProgramming/SimpleServer/browser.rb
+++ RubyProgramming/SimpleServer/browser.rb
@@ -1,32 +1,30 @@
-require 'socket'
+require 'socket' 
+require 'json'       
+ 
+host = 'localhost'    
+port = 2000                         
 
-host = "localhost" #The Web Server
-port = 2000
+puts "What's your request type? GET/POST"
+request_type = gets.chomp.upcase
+puts "What path are you looking for?"
+path = gets.chomp.downcase
 
-
-#This is the HTTP request we sent to fetch a file
-#request = "Get #{path} HTTP/1.0\r\n\r\n"
-
-puts "What type of request do you want to make, POST or GET?"
-request_path = gets.chomp.downcase
-
-if request_path == "GET"
-	request = "Get #{path} HTTP/1.0\r\n\r\n"
-
-elsif request_path == "POST"
-	puts "What is your name?"
-	name = gets.chomp
-	puts "What is your email?"
-	email = gets.chomp
-
-	viking ={:viking => {:name=> name, :email=> email}}
-
-
+if request_type == "GET"
+  request = "GET #{path} HTTP/1.0\r\n\r\n"  
+elsif request_type == "POST"
+  puts "What name a viking has?"
+  name = gets.chomp
+  puts "What's his email?"
+  email = gets.chomp
+  viking_hash = {:viking => {:name => name, :email => email}}
+  content = viking_hash.to_json
+  request = "POST #{path} HTTP/1.0\nContent-Length: #{content.size}\r\n\r\n#{content}"
+end
 
 
-socket = TCPSocket.open(host, port) # Connect to Server
-socket.print(request) #send request
-response = socket.read
-# Split response at first blank line into headers and body
-headers,body = response.split("\r\n\r\n", 2)
-print body #And display it
+socket = TCPSocket.open(host,port)  
+socket.print(request)               
+response = socket.read              
+
+headers,body = response.split("\r\n\r\n", 2) 
+print body  
