d823f7e04232001ee2c3f31cc8299211be28c230
williamkennedy <williamkennedyjnr@gmail.com>
2015-02-02 21:24:27 +0000

    finished fibs but only half finished merge sort

--- RubyProgramming/Recursion/mergeSort.rb
+++ RubyProgramming/Recursion/mergeSort.rb
@@ -1,11 +1,33 @@
-def merge_sort(arr)
-	return arr if arr.length == 1
-	mid = arr.length / 2
-	left = merge_sort(arr[0..mid-1])
-	right = merge_sort(arr[mid..-1])
-	merge(left,right)
-
+def mergesort(array)
+  def merge(left_sorted, right_sorted)
+    res = []
+    l = 0
+    r = 0
+ 
+    loop do
+      break if r >= right_sorted.length and l >= left_sorted.length
+ 
+      if r >= right_sorted.length or (l < left_sorted.length and left_sorted[l] < right_sorted[r])
+        res << left_sorted[l]
+        l += 1
+      else
+        res << right_sorted[r]
+        r += 1
+      end
+    end
+ 
+    return res
+  end
+ 
+  def mergesort_iter(array_sliced)
+    return array_sliced if array_sliced.length <= 1
+ 
+    mid = array_sliced.length/2 - 1
+    left_sorted = mergesort_iter(array_sliced[0..mid])
+    right_sorted = mergesort_iter(array_sliced[mid+1..-1])
+    return merge(left_sorted, right_sorted)
+  end
+ 
+  mergesort_iter(array)
 end
-
-puts merge_sort([1,7,6,5,4,3,2,1])
-#half findished the merge sort, will need to complete later. Having difficulty following the psudocode on http://en.wikipedia.org/wiki/Merge_sort
+puts mergesort([10,9,8,-300,250,1,7,6,5,4,3,2,1])
